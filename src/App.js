4c2f4e4ff9a1af55f14e0161c9d195b9c6df769f
Stanley Brown <97811857+fullstackstan@users.noreply.github.com>
2022-07-05 18:07:34 -0500

    2.4 complete

--- src/App.js
+++ src/App.js
@@ -1,54 +1,54 @@
 const App = () => {
-  const course = {
-    id: 1,
-    name: "Half Stack application development",
-    parts: [
-      {
-        name: "Fundamentals of React",
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: "Using props to pass data",
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: "Using props to pass data",
-        exercises: 7,
-        id: 5
-      },
-      {
-        name: "Using props to pass data",
-        exercises: 7,
-        id: 6
-      },
-      {
-        name: "Using props to pass data",
-        exercises: 7,
-        id: 91
-      },
-      {
-        name: "Using props to pass data",
-        exercises: 37,
-        id: 90
-      },
-      {
-        name: "Using props to pass data",
-        exercises: 17,
-        id: 97
-      },
-      {
-        name: "State of a component",
-        exercises: 24,
-        id: 3
-      }
-    ]
-  };
+  const courses = [
+    {
+      name: "Half Stack application development",
+      id: 1,
+      parts: [
+        {
+          name: "Fundamentals of React",
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: "Using props to pass data",
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: "State of a component",
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: "Redux",
+          exercises: 211,
+          id: 4
+        }
+      ]
+    },
+    {
+      name: "Node.js",
+      id: 2,
+      parts: [
+        {
+          name: "Routing",
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: "Middlewares",
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ];
 
   return (
     <div>
-      <Course course={course} />
+      {courses.map((course) => (
+        <Course course={course} />
+      ))}
     </div>
   );
   // <Course course={course} />
